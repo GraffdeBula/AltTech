7145481d122bdfa4459156b35f0e2b67864ce53d
Andrey <adbulavskiy@gmail.com>
2023-12-17 21:57:57 +0700

    Create settings for app (folder name)

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
  *  запускает AdminController
  */
 header('Content-Type: text/html; charset=utf8');
+//подключение настроек
+include_once "app/Settings.php";
+
 //подключение моего автолоадера
-include_once "{$_SERVER['DOCUMENT_ROOT']}/AltTech/app/autoloader.php";
+include_once "{$_SERVER['DOCUMENT_ROOT']}/".WORK_FOLDER."/app/autoloader.php";
 spl_autoload_register([new autoloader(), 'getClass']);
 
 //подключение composer
-require_once "{$_SERVER['DOCUMENT_ROOT']}/AltTech/vendor/autoload.php";
+require_once "{$_SERVER['DOCUMENT_ROOT']}/".WORK_FOLDER."/vendor/autoload.php";
 
 //запуск обработчика ошибок WHOOPS
 $whoops = new \Whoops\Run;
