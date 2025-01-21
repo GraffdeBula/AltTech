e060ecc0df0fddba3d2e8761380d50902648c5ee
AndreyBulavskiy <adbulavskiy@gmail.com>
2025-01-21 12:33:03 +0700

    new fields in ContP1 report

--- views_new/reports/ContP1Rep.php
+++ views_new/reports/ContP1Rep.php
@@ -27,34 +27,34 @@
                         <th scope='col'>ContCode</th>
                         <th scope='col'>ФИО</th>
                         <th scope='col'>Подразделение</th>
-                        <th scope='col'>Менеджер</th>
-                        <th scope='col'>Долг по ЭПЭ</th>
+                        <th scope='col'>Менеджер</th>                        
                         <th scope='col'>Дата договора</th>
                         <th scope='col'>Дата первого платежа</th>
                         <th scope='col'>Программа</th>
                         <th scope='col'>Тариф</th>
-                        <th scope='col'>Сумма</th>
+                        <th scope='col'>Сумма договора</th>
                         <th scope='col'>Скидка</th>
+                        <th scope='col'>Число платежей</th>
+                        <th scope='col'>Внесено по договору</th>                        
                     </tr>
                 </thead>
                 <tbody>                    
                 <?php
-                    foreach ($Report as $Cont){
-                        $ContDate=(new PrintFunctions())->DateToStr($Cont->FRCONTDATE);
-                        $PayDate=(new PrintFunctions())->DateToStr($Cont->PAYDATE);
+                    foreach ($Report as $Cont){                        
                         echo("<tr class='table-secondary'>");
                         echo("<td><a target='_blank' href='index.php?controller=ATClientFileCtrl&ClCode={$Cont->CLCODE}'>{$Cont->CLCODE}</a></td>");                        
                         echo("<td><a target='_blank' href='index.php?controller=ATContP1FileFrontCtrl&ClCode={$Cont->CLCODE}&ContCode={$Cont->CONTCODE}'>{$Cont->CONTCODE}</a></td>");
                         echo("<td>$Cont->CLFIO</td>");
                         echo("<td>{$Cont->FROFFICE}</td>");
-                        echo("<td>{$Cont->FRPERSMANAGER}</td>");
-                        echo str_replace('.',',',"<td>{$Cont->EXTOTDEBTSUM}</td>");
-                        echo("<td>{$ContDate}</td>");
-                        echo("<td>{$PayDate}</td>");
+                        echo("<td>{$Cont->FRPERSMANAGER}</td>");                        
+                        echo("<td>".(new PrintFunctions())->DateToStr($Cont->FRCONTDATE)."</td>");
+                        echo("<td>".(new PrintFunctions())->DateToStr($Cont->PAYDATE)."</td>");
                         echo("<td>{$Cont->FRCONTPROG}</td>");
                         echo("<td>{$Cont->FRCONTTARIF}</td>");
                         echo str_replace('.',',',"<td>{$Cont->FRCONTSUM}</td>");
                         echo str_replace('.',',',"<td>{$Cont->DISCOUNTSUM}</td>");
+                        echo ("<td>{$Cont->PAYNUM}</td>");
+                        echo ("<td>".$Cont->PAYSUM."</td>");
                         echo("<tr>");
                     }                    
                 ?>
