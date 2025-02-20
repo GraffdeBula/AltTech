d98b3f23e186ffc1f115854a617a0cea715b31d3
AndreyBulavskiy <adbulavskiy@gmail.com>
2025-02-20 16:48:11 +0700

    new manager rules

--- domain/Branch.php
+++ domain/Branch.php
@@ -6,12 +6,15 @@
  * @author Andrey
  */
 class Branch{
+    protected $BrName;
     protected $Rec;
     protected $BranchList;
     
     public function __construct($BrName=''){
+        $this->BrName=$BrName;
         $this->Rec=(new BranchRecMod())->getBranchByName($BrName);
         $this->BranchList=(new BranchRecMod())->getBranchList();
+        $this->ChangeDirForDoc();
     }
     
     public function getRec(){
@@ -20,5 +23,11 @@ class Branch{
 
     public function getBranchList(){
         return $this->BranchList;
+    }
+    
+    protected function ChangeDirForDoc(){        
+        if (($this->BrName=='ОП Центральный')&&(in_array($_SESSION['EmName'],['Илья Ковтун','Мария Мусатова']))){
+            $this->Rec->BRDIR=$_SESSION['EmName'];            
+        }
     }
 }
