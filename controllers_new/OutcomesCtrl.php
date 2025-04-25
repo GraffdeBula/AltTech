e0273c70981fe098f5d9c8cc27969975478dc660
AndreyBulavskiy <adbulavskiy@gmail.com>
2025-04-25 15:12:46 +0700

    Outcomes

---
+++ controllers_new/OutcomesCtrl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * контроллер для работы с вьюшкой Амо
+ *
+ * @author Andrey
+ */
+class OutcomesCtrl extends ControllerMain{
+    protected $OutcomesList=[];  
+    protected $TotalOutcomes=0;
+    protected $TotalIncomes=0;
+    
+    public function actionIndex(){
+        $this->ViewName='Учёт расходов';
+        
+        $this->getOutcomes();
+        
+        $this->render('Outcomes',['OutcomeList'=>$this->OutcomesList,'TotalOutcomes'=>$this->TotalOutcomes,'TotalIncomes'=>$this->TotalIncomes]);
+    }
+    
+    public function actionAddOutcome(){
+        $Sql='INSERT INTO tbl6Outcomes (OutDate,OutSum,Outcome) VALUES (?,?,?)';
+        db2::getInstance()->Query($Sql,[$_GET['OutDate'],$_GET['OutSum'],$_GET['Outcome']]);
+        header("Location: index_admin.php?controller=OutcomesCtrl");
+    }
+    
+    protected function getOutcomes(){
+        $Sql='SELECT OutDate,OutSum,Outcome FROM tbl6Outcomes ORDER BY Id DESC';
+        $this->OutcomesList=db2::getInstance()->FetchAll($Sql,[]);
+        
+        $Sql='SELECT SUM(OutSum) AS TotSum FROM tbl6Outcomes WHERE OutDate BETWEEN ? AND ?';
+        $this->TotalOutcomes=db2::getInstance()->FetchOne($Sql,['01.04.2025','30.04.2025'])->TOTSUM;
+        
+        $Sql='SELECT SUM(PaySUM) AS TotSum FROM tbl5Payments WHERE (PayDate BETWEEN ? AND ?) AND ContBranch=? AND PayType<?';
+        $this->TotalIncomes=db2::getInstance()->FetchOne($Sql,['01.04.2025','30.04.2025','ОП Томск',10])->TOTSUM;
+    }
+}
--- views_new/ATMainForm.php
+++ views_new/ATMainForm.php
@@ -33,7 +33,12 @@
               <a class='nav-link' data-bs-toggle='tab' href='#reports'>Отчёты</a>
             </li>"); 
         }        
-        
+        if ((in_array($_SESSION['EmRole'],['admin','top']))or(in_array($_SESSION['EmName'],['Андрей Догаев']))){
+            echo("
+            <li class='nav-item'>
+              <a class='nav-link' data-bs-toggle='tab' href='#outcomes'>Расходы</a>
+            </li>");
+        }
         if (in_array($_SESSION['EmRole'],['admin','top'])){
             echo("
             <li class='nav-item'>
@@ -312,6 +317,9 @@
             </div>
             
         </div><!--отчёты-->   
+        <div class="tab-pane fade" id="outcomes"> 
+            <a target="_blank" href="index_admin.php?controller=OutcomesCtrl"><button class='btn btn-info'>ОТКРЫТЬ УЧЁТ РАСХОДОВ</button></a>
+        </div>
         <div class="tab-pane fade" id="discounts"> 
             <?php
                 foreach($DiscList as $key=>$Cont){
