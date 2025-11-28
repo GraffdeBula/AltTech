4866a7937550db6ea4dcaa698becfb0179721792
AndreyBulavskiy <adbulavskiy@gmail.com>
2025-11-28 17:50:43 +0700

    add OutcomePayType

--- controllers_new/OutcomesCtrl.php
+++ controllers_new/OutcomesCtrl.php
@@ -8,29 +8,69 @@ class OutcomesCtrl extends ControllerMain{
     protected $OutcomesList=[];  
     protected $TotalOutcomes=0;
     protected $TotalIncomes=0;
+    protected $OutcomeDr;
+    
     
     public function actionIndex(){
         $this->ViewName='Учёт расходов';
         
-        $this->getOutcomes();
+        if (isset($_GET['BranchName'])) {
+            $Branch=$_GET['BranchName'];
+        }else{
+            $Branch=$_SESSION['EmBranch'];
+        }
+        if (isset($_GET['DateF'])){
+            $DateF=$_GET['DateF'];
+        }else{
+            $DateF=date('Y-m-01');
+        }
+        if (isset($_GET['DateL'])){
+            $DateL=$_GET['DateL'];
+        }else{
+            $LastDay='31';
+            if (in_array(date('m'),[4,6,9,11])){
+                $LastDay='30';
+            }elseif(in_array(date('m'),[2,])){
+                $LastDay='28';
+            }
+            $DateL=date('Y-m-'.$LastDay);
+        }
+        $this->getOutcomes($Branch,$DateF,$DateL);
         
-        $this->render('Outcomes',['OutcomeList'=>$this->OutcomesList,'TotalOutcomes'=>$this->TotalOutcomes,'TotalIncomes'=>$this->TotalIncomes]);
+        $this->render('Outcomes',
+            ['OutcomeList'=>$this->OutcomesList,
+                'TotalOutcomes'=>$this->TotalOutcomes,
+                'TotalIncomes'=>$this->TotalIncomes,
+                'OutcomeDr'=>$this->OutcomeDr,
+                'Dates'=>[$DateF,$DateL],
+            ]);
     }
-    
+    /*
+     */
     public function actionAddOutcome(){
-        $Sql='INSERT INTO tbl6Outcomes (OutDate,OutSum,Outcome) VALUES (?,?,?)';
-        db2::getInstance()->Query($Sql,[$_GET['OutDate'],$_GET['OutSum'],$_GET['Outcome']]);
+        $Sql='INSERT INTO tbl6Outcomes (OutBranch,OutDate,OutSum,Outcome,Comment,OutcomeType) VALUES (?,?,?,?,?,?)';
+        db2::getInstance()->Query($Sql,[$_GET['OutBranch'],$_GET['OutDate'],$_GET['OutSum'],$_GET['Outcome'],$_GET['Comment'],$_GET['OutcomeType']]);
         header("Location: index_admin.php?controller=OutcomesCtrl");
     }
     
-    protected function getOutcomes(){
-        $Sql='SELECT ID,OutDate,OutSum,Outcome FROM tbl6Outcomes ORDER BY Id DESC';
-        $this->OutcomesList=db2::getInstance()->FetchAll($Sql,[]);
+    protected function getOutcomes($Branch,$DateF,$DateL){
+        $Sql='SELECT * FROM tbl6DROutcomes ORDER BY Id DESC';
+        $this->OutcomeDr=db2::getInstance()->FetchAll($Sql,[]);
         
-        $Sql='SELECT SUM(OutSum) AS TotSum FROM tbl6Outcomes WHERE OutDate BETWEEN ? AND ?';
-        $this->TotalOutcomes=db2::getInstance()->FetchOne($Sql,['01.04.2025','30.04.2025'])->TOTSUM;
+        $Sql='SELECT * FROM tbl6Outcomes WHERE OutBranch=? AND (OutDate BETWEEN ? AND ?) ORDER BY Id DESC';
+        $this->OutcomesList=db2::getInstance()->FetchAll($Sql,[$Branch,$DateF,$DateL]);
         
-        $Sql='SELECT SUM(PaySUM) AS TotSum FROM tbl5Payments WHERE (PayDate BETWEEN ? AND ?) AND ContBranch=? AND PayType<?';
-        $this->TotalIncomes=db2::getInstance()->FetchOne($Sql,['01.04.2025','30.04.2025','ОП Томск',10])->TOTSUM;
+        $Sql='SELECT SUM(OutSum) AS TotSum FROM tbl6Outcomes WHERE OutBranch=? AND (OutDate BETWEEN ? AND ?)';
+        $this->TotalOutcomes=0;
+        if (isset(db2::getInstance()->FetchOne($Sql,[$Branch,$DateF,$DateL])->TOTSUM)){
+            $this->TotalOutcomes=db2::getInstance()->FetchOne($Sql,[$Branch,$DateF,$DateL])->TOTSUM;
+        }
+        $Sql='SELECT SUM(PaySUM) AS TotSum FROM tbl5Payments WHERE ContBranch=? AND (PayDate BETWEEN ? AND ?) AND PayType>?';
+        $this->TotalIncomes=0;
+        if (isset(db2::getInstance()->FetchOne($Sql,[$Branch,$DateF,$DateL,10])->TOTSUM)){
+            $this->TotalIncomes=db2::getInstance()->FetchOne($Sql,[$Branch,$DateF,$DateL,10])->TOTSUM;
+        }
     }
+    
+    
 }
--- views_new/Outcomes.php
+++ views_new/Outcomes.php
@@ -2,7 +2,6 @@
 /*
  * для внесения расходов в таблицу
  *  */
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,22 +9,53 @@
 
 </head>
 <body>
-    <h1>Учёт расходов</h1>
-    <h2>Финаносвый результат</h2>
+    <h2>Учёт расходов</h2>
+    <a style="pointer-events: none;" href='downloads/Расходы.xlsx'><button  type='button' class='btn btn-success disabled'>В EXCEL</button></a>
+        <form>            
+            <?php   
+                if ($_SESSION['EmRole']=='top'){
+                    echo("<label>Филиал</label>");
+                    (new EchoBranchList())->echoList('','BranchName');
+                } else {
+                    echo("<input type='hidden' name='BranchName' value='{$_SESSION['EmBranch']}'>");
+                }
+                (new MyForm('OutcomesCtrl',''))->AddForm2();
+            ?>
+            <label>Показать расходы от </label><input type='date' name='DateF'>
+            <label> до </label><input type='date' name='DateL'> 
+            <button class='btn btn-info'>Сформировать спиок</button>
+        </form>
+    <h5><u>Финансовый результат</u></h5>
     <?php
         $Total=$TotalIncomes-$TotalOutcomes;
         echo("Доходы всего: ".$TotalIncomes);
         echo("<br>Расходы всего: ".$TotalOutcomes);
         echo("<br>Итого финрез: ".$Total);
     ?>
-    <h2>Внесение расходов</h2>
-    <form>
+    <h5><u>Внесение расходов</u></h5>
+    <form autocomplete='off'>
         <?php
             (new MyForm('OutcomesCtrl', 'AddOutcome'))->AddForm2();
         ?>
-        <label>Расход</label><input type="text" name='Outcome'><!-- какой расход -->            
+        <label>Расход</label><select type="text" name='Outcome'><!-- расход по справочнику --> 
+            <option></option>
+            <?php
+                foreach($OutcomeDr as $Outcome){
+                    echo("<option value='$Outcome->OUTCOME'>$Outcome->OUTCOME</option>");
+                }
+            ?>
+        </select>
+        <label>Способ оплаты</label><select type="text" name='OutcomeType'><!-- способ оплаты --> 
+            <option></option>
+            <option>с расчётного счёта</option>
+            <option>наличными</option>
+            <option>другими наличными</option>
+        </select>
+        <br>
+        <label>Комментарий</label><input type="text" size='80' name='Comment'><!-- расшифровка расхода -->  
         <label>Сумма</label><input type="number" name='OutSum'><!-- сумма -->
         <label>Дата</label><input type="date" name="OutDate"><!-- дата расхода -->
+        <input type="hidden" name="OutBranch" value="<?=$_SESSION['EmBranch']?>">
         <button class="btn btn-primary">ДОБАВИТЬ</button>
     </form>
     <div class="row">
@@ -34,21 +64,31 @@
                 <tr>
                     <th scope="col"> </th>
                     <th scope="col">ID</th>
+                    <th scope="col">Филиал</th>
                     <th scope="col">Дата</th>
                     <th scope="col">Сумма</th>
-                    <th scope="col">Расход</th>                                
+                    <th scope="col">Способ оплаты</th>
+                    <th scope="col">Расход</th>
+                    <th scope="col">Комментарий</th>
                 </tr>
             </thead>
             <tbody>
             <?php 
-                foreach($OutcomeList AS $key=>$Outcome){ 
-                    //$ContDate=(new PrintFunctions())->DateToStr($Cont->FRCONTDATE);
-                    echo("<tr class='table-info'>"                        
+                foreach($OutcomeList AS $key=>$Outcome){                     
+                    if ((new DateTime($Outcome->OUTDATE))==(new DateTime($Outcome->OUTFACTDATE))){
+                        $OutType='table-info';
+                    }else{
+                        $OutType='table-warning';
+                    }
+                    echo("<tr class='{$OutType}'>"                        
                         ."<td> </td>"
                         ."<td>$Outcome->ID</td>"
+                        ."<td>$Outcome->OUTBRANCH</td>"
                         ."<td>".(new PrintFunctions())->DateToStr($Outcome->OUTDATE)."</td>"
                         ."<td>$Outcome->OUTSUM</td>"
+                        ."<td>$Outcome->OUTCOMETYPE</td>"
                         ."<td>$Outcome->OUTCOME</td>"
+                        ."<td>$Outcome->COMMENT</td>"
                     ."</tr>");
 
                 }
